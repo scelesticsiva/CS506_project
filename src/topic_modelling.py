ec4a82a13f8e96373ea8eacbc94f8aab79cd2576
Sivaramakrishnan Sankarapandian <scelesticsiva@gmail.com>
2018-04-10 20:12:37 -0400

    updated topic file

--- src/topic_modelling.py
+++ src/topic_modelling.py
@@ -11,13 +11,14 @@ from nltk.corpus import stopwords
 from gensim import corpora,models
 import json
 from collections import defaultdict
+import numpy as np
 
-NO_OF_WORDS_IN_EACH_REVIEW_THRESHOLD = 3
-NO_OF_SENTENCES_IN_EACH_REVIEW_THRESHOLD = 7
+NO_OF_WORDS_IN_EACH_SENTENCE_THRESHOLD = 10
+NO_OF_SENTENCES_IN_EACH_REVIEW_THRESHOLD = 5
 NO_OF_TOPICS = 2
 
 YELP_DATASET_PATH = "/Users/siva/Documents/CS506/datasets/yelp_dataset/review.json"
-SAVING_FILE_PATH = "reviews.json"
+SAVING_FILE_PATH = "results"
 
 def read_restaurant_reviews(FILE_PATH):
     business_reviews = defaultdict(list)
@@ -27,6 +28,8 @@ def read_restaurant_reviews(FILE_PATH):
             business_reviews[review["business_id"]].append(review["text"])
             if i % 1000000 == 0:
                 print(i)
+    print(sum([1 for e in business_reviews if len(business_reviews[e])>100]))
+    print(len(business_reviews))
     return business_reviews
 
 def create_topics_dictionary(business_reviews):
@@ -34,11 +37,13 @@ def create_topics_dictionary(business_reviews):
     stop_words = set(stopwords.words("english"))
     #stemmer = PorterStemmer()
     restaurants_with_topics = {}
+    count = 0
     
     for i,each_business in enumerate(business_reviews):
-        if i % 10 == 0 and i != 0:
-            break
+        if count % 10 == 0:
+            print(count)
         if len(business_reviews[each_business]) > 100:
+            count += 1
             reviews_with_topics = defaultdict(list)
             for each_review in business_reviews[each_business]:
                 sentences_each_review = each_review.split(".")
@@ -46,7 +51,7 @@ def create_topics_dictionary(business_reviews):
                 for each_sentence in sentences_each_review:
                     tokens = tokenizer.tokenize(each_sentence.replace("\n","").lower())
                     tokens = [e for e in tokens if e not in stop_words]
-                    if len(tokens)>NO_OF_WORDS_IN_EACH_REVIEW_THRESHOLD:
+                    if len(tokens)>NO_OF_WORDS_IN_EACH_SENTENCE_THRESHOLD:
                         texts.append(tokens)
                 if len(texts) > NO_OF_SENTENCES_IN_EACH_REVIEW_THRESHOLD:
                     dictionary = corpora.Dictionary(texts)
@@ -58,9 +63,11 @@ def create_topics_dictionary(business_reviews):
     return restaurants_with_topics
     
 def save_topics_reviews(restaurants_with_topics):
-    with open(SAVING_FILE_PATH,"w") as write_json:
-        for each in restaurants_with_topics:
-            json.dump(restaurants_with_topics[each],write_json)
+    np.save(SAVING_FILE_PATH,restaurants_with_topics)
+#    with open(SAVING_FILE_PATH,"w") as write_json:
+#        for each in restaurants_with_topics:
+#            print(restaurants_with_topics[each])
+#            json.dump(restaurants_with_topics[each],write_json)
         
 def main():
     business_reviews = read_restaurant_reviews(YELP_DATASET_PATH)
